f0669d4229406ddba852c22920501a993ba4f61c
Chris Sorros <72184260+chrissorros@users.noreply.github.com>
2023-03-19 04:59:23 +0000

    Got count working

--- run.py
+++ run.py
@@ -1,27 +1,35 @@
 import numpy as np
 from enum import Enum
 
-
+global numofcards
 class Cards(Enum):
-    A = {'card': 1, 'value': [1, 11]}
-    TWO = {'card': 2, 'value': 2}
-    THREE = {'card': 3, 'value': 3}
-    FOUR = {'card': 4, 'value': 4}
-    FIVE = {'card': 5, 'value': 5}
-    SIX = {'card': 6, 'value': 6}
-    SEVEN = {'card': 7, 'value': 7}
-    EIGHT = {'card': 8, 'value': 8}
-    NINE = {'card': 9, 'value': 9}
-    TEN = {'card': 10, 'value': 10}
-    J = {'card': 11, 'value': 10}
-    Q = {'card': 12, 'value': 10}
-    K = {'card': 13, 'value': 10}
+    A = {'card': 1, 'value': [1,11]}
+    TWO = {'card': 2, 'value': [2]}
+    THREE = {'card': 3, 'value': [3]}
+    FOUR = {'card': 4, 'value': [4]}
+    FIVE = {'card': 5, 'value': [5]}
+    SIX = {'card': 6, 'value': [6]}
+    SEVEN = {'card': 7, 'value': [7]}
+    EIGHT = {'card': 8, 'value': [8]}
+    NINE = {'card': 9, 'value': [9]}
+    TEN = {'card': 10, 'value': [10]}
+    J = {'card': 11, 'value': [10]}
+    Q = {'card': 12, 'value': [10]}
+    K = {'card': 13, 'value': [10]}
+
+
+def __getitem__(self, item):
+    return self._data['value']
 
 def cards(card):
     #match the card name string to the enum
     return [Cards[x.upper()] for x in card] if isinstance(card, list) else Cards[card.upper()]
 #input number of decks being played on the game
-numofdecks = int(input("Number of decks being used on this game: "))
+while True:
+    numofdecks = int(input("Number of decks being used on this game: ")) 
+    if numofdecks in [1,2,4,6,8]:
+        break
+
 
 deck = {
     Cards.A: 4*numofdecks,
@@ -44,6 +52,7 @@ deck = {
 numofcards = numofdecks * 52
 #running count
 count = 0
+that=0
 #track cards that were previusly played on the game
 yourhand = []
 yourcount=0
@@ -56,42 +65,78 @@ dealercount=0
 # When you see an 8: Do nothing(Worth 0)
 # When you see a 2 or 7: Add 0.5 to your running count
 # When you see a 3, 4, 5, or 6: Add 1 to your running count
-def cardcount(dealt):
+def cardcount(dealt,cnt):
     decksLeft = int(numofcards % 52)
-    if dealt == Cards.TWO["card"] or dealt == Cards.SEVEN["card"]:
-        count = + 0.5
-    elif dealt == Cards.THREE["card"] or dealt == Cards.FOUR["card"] or dealt == Cards.SIX["card"]:
-        count = + 1
-    elif dealt == Cards.FIVE["card"]:
-        count = + 1.5
-    elif dealt == Cards.EIGHT["card"]:
-        count = + 0
-    elif dealt == Cards.NINE["card"]:
-        count = - 0.5
-    elif dealt == Cards.ACE["card"] or dealt == Cards.TEN["card"] or dealt == Cards.JACK["card"] or dealt == Cards.QUEEN["card"] or dealt == Cards.KING["card"]:
-        count = - 1
-    return count/decksLeft
+    if dealt == Cards.TWO.value["card"] or dealt == Cards.SEVEN.value["card"]:
+        cnt = + 0.5
+    elif dealt == Cards.THREE.value["card"] or dealt == Cards.FOUR.value["card"] or dealt == Cards.SIX.value["card"]:
+        cnt = + 1
+    elif dealt == Cards.FIVE.value["card"]:
+        cnt = + 1.5
+    elif dealt == Cards.NINE.value["card"]:
+        cnt = - 0.5
+    elif dealt == Cards.A.value["card"] or dealt == Cards.TEN.value["card"] or dealt == Cards.J.value["card"] or dealt == Cards.Q.value["card"] or dealt == Cards.K.value["card"]:
+        cnt = - 1
+    return cnt/decksLeft,cnt
 
-def dothatinC():
-    total= sum([x.value["value"] for x in deck if (x.value["value"][])>=(21-yourcount)])
-    print(total)
+def dothathing():
+    total=0
+    for x in deck:
+        if x.value["value"][0] <= 21-yourcount:
+            print(f"| {x.name} |", end="")
+            total+=deck[x]
+    print(f"Total number of viable Cards left {total}")
 
+#update the deck with the cards drawn
+def updatedeck(cards,numcards):
+    CRED = '\033[91m'
+    CEND = '\033[0m'
+    if isinstance(cards, list):
+        numcards-=len(cards)
+        for x in cards:
+            deck[x]-=1
+            if deck[x]<0:print(f"{CRED}WARNING: {x.name} is less than 0{CEND}")
+    else:
+        numcards-=1
+        deck[cards]-=1
+        if deck[cards]<0:print(f"{CRED}WARNING: {cards.name} is less than 0{CEND}")
+        
+    print(f"Number of Cards left {numcards}")
 
-#create class to tell how many copies of each card there are
-# class Deck():
-
-#make dictionary to hold the amount of each Cards there are using the enum
-
-
-
-while(True):
-    yourhand = cards(input("Initial User Cards: ").split())
-    print(yourhand)
-    yourcount=sum([x.value["value"] for x in yourhand])
-    print(yourcount)
-    dothathing()
-    # usercards = Cards(cardsIn)
-    # dealercards= Cards(input("Initial Dealer Card: "))
-    # othercards= Cards(input("Other cards on the table: ").split())
+    return numcards
     
+
+while True: #Round Loop
+    while True: #input error check
+        temp = input("Initial User Cards: ").split()
+        if(all(x in [member.name for member in Cards] for x in temp)):
+            yourhand=cards(temp)
+            break
+    numofcards=updatedeck(yourhand,numofcards)
+    for x in yourhand:
+        that,count=cardcount(x,count)
+    yourcount=sum([x.value["value"][0] for x in yourhand])
+    dothathing()
+    while True:   #input error check
+        temp= input("Initial Dealer Card: ")
+        if temp in [member.name for member in Cards]:
+            dealershand=cards(temp)
+            break
+
+    numofcards=updatedeck(dealershand,numofcards)
+    that,count=cardcount(dealershand,count)
+    dealercount=dealershand.value["value"][0]
+    other=[]
+    while True:   #input error check
+        temp = input("Other Cards: ").split()
+        if(all(x in [member.name for member in Cards] for x in temp)):
+            other=cards(temp)
+            break
+    
+    numofcards=updatedeck(other,numofcards)
+    for x in other:
+        that,count=cardcount(x,count)
+    print(f"The advantage we have is {that}")
     # cardcount(dealt)
+
+    
