339f2d6841c8a0663d0f16760f1dad36c63a10f7
MikePlekan <70487027+MikePlekan@users.noreply.github.com>
2023-12-12 20:18:54 +0000

    added num of times

--- analysis.py
+++ analysis.py
@@ -9,12 +9,13 @@ with open('game.csv', mode='r') as infile:
         count=float(row[0])
         win=float(row[1])
         #update the avgerage if the key is already in the dictionary else add the key and value
-        data.update({count: (data[count]+win)/2 if count in data else win})
-#sort the dictionary by key and print the result
+        data.update({count:[(data[count][0]+win)/2 if count in data else win, data[count][1]+1 if count in data else 1]})
+print("Done")
+#sort the dictionary by the key
 #create csv file
 with open('analysis.csv', mode='w') as outfile:
     writer = csv.writer(outfile)
-    writer.writerow(['Count', 'Average'])
+    writer.writerow(['Count', 'Average', 'Number of times'])
     for key in sorted(data.keys()):
-        writer.writerow([key, data[key]])
+        writer.writerow([key, data[key][0], data[key][1]])
         
